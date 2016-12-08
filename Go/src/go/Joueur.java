285cfa11d1b3bee1ef20671dfa7a48cec57f6604
RugessNome <vincent.chambrin@gmail.com>
2016-12-08 10:01:26 +0100

    Ajout de l'attribut nbrPierreCapturees

--- Go/src/go/Joueur.java
+++ Go/src/go/Joueur.java
@@ -7,8 +7,29 @@ package go;
 
 /**
  *
- * @author thibault
+ * @author Vincent
  */
 public class Joueur {
+    /*
+    Le nombre de pierre que le joueur a capturé.
+    */
+    private int nbrPierreCapturees;
+    
+    /**
+     * Initialise le nombre de pierres capturees à 0.
+     */
+    public Joueur()
+    {
+        nbrPierreCapturees = 0;
+    }
+
+    public int getNbrPierreCapturees() {
+        return nbrPierreCapturees;
+    }
+
+    public void setNbrPierreCapturees(int nbrPierreCapturees) {
+        this.nbrPierreCapturees = nbrPierreCapturees;
+    }
+    
     
 }
